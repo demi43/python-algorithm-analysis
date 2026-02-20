745c5d01708d0d527a9efd64b2e0c92f0df906fb
OLAOLUWA ADEDAMOLA OMODEMI <157908846+demi43@users.noreply.github.com>
2026-02-20 17:03:25 -0500

    Implement brute force method for subset sum problem
    Added brute force logic to find disjoint subsets with equal sum.

--- Partition.py
+++ Partition.py
@@ -14,3 +14,13 @@
 # 3. If Option 1 is selected:
 #     a. prompt user for a string   
 # 4. If Option 2 is selected:
+
+# trying to figure out the best brute force way to solve the question 
+sum=0
+result[][]=[]
+for i in range(0, len(digits)):
+  sum+=digits[i]
+if sum % 2==0:
+  
+else:
+  print("No disjoint subsets with the smae sum of their elements found")
